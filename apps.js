d0676a119dbbe435f03c76715175fad31187af3e
Kolos Farkas <kolos.farkas@betssongroup.com>
2022-11-21 01:10:54 +0100

    lines/bresenham

--- apps.js
+++ apps.js
@@ -2,4 +2,5 @@ const APPS = [
 	["3D Cube 45deg", "cube45.html"],
 	["3D Cube skeleton 45deg", "cube-skeleton45.html"],
 	["2D triangle (div)", "triangle.html"],
+	["2D Lines (canvas)", "lines.html"]
 ];
